118b065afa71f1722900bce0e9e987b0637ee94c
Elshod Musurmonov <elshodatc1116@gmail.com>
2026-03-28 23:47:11 +0500

    Save

--- routes/console.php
+++ routes/console.php
@@ -4,9 +4,7 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Schedule::command('queue:work --stop-when-empty')->everyMinute()->withoutOverlapping();
 
 // * * * * * cd /path-to-your-project && php artisan schedule:run >> /dev/null 2>&1
 // Serverda Cron Jobni sozlash
