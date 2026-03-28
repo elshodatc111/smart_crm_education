2290b67280b270a382e55e49d39e78e28cf125c4
Elshod Musurmonov <elshodatc1116@gmail.com>
2026-03-28 17:11:20 +0500

    Save

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,41 +12,81 @@
   </div>
   <section class="section dashboard">
     <div class="row">
-
       <div class="col-lg-12">
         <div class="card">
           <div class="card-body">
-            <h5 class="card-title">Hush kelibsiz</h5>
-            <p>Siz tizimga muvaffaqiyatli kirdingiz. Bu yerda sizning asosiy statistikalaringiz ko'rinadi.</p>
+            <h5 class="card-title">Dars jadvali</h5>
+            <table class="table table-bordered" style="font-size: 14px">
+              <head>
+                <tr class="text-center">
+                  <th>Dars Vaqt \ Hafta kunlari</th>
+                  <th>Dushanba</th>
+                  <th>Seshanba</th>
+                  <th>Chorshanba</th>
+                  <th>Payshanba</th>
+                  <th>Juma</th>
+                  <th>Shanba</th>
+                </tr>
+              </head>
+              <tbody class="text-center">
+                <tr>
+                  <th>08:00 - 09:30</th>
+                  <td>
+                    <span class="badge rounded-pill bg-primary">Matematika</span>
+                    <span class="badge rounded-pill bg-primary">FIZIKA</span>
+                  </td>
+                  <td></td>
+                  <td>
+                    <span class="badge rounded-pill bg-primary">Tarix</span>
+                  </td>
+                  <td>
+                    <span class="badge rounded-pill bg-primary">Tarix</span>
+                    <span class="badge rounded-pill bg-primary">Falsafa</span>
+                    <span class="badge rounded-pill bg-primary">Informatika</span>
+                  </td>
+                  <td></td>
+                  <td>
+                    <span class="badge rounded-pill bg-primary">Tarix</span>
+                  </td>
+                </tr>
+                <tr>
+                  <th>09:30 - 11:00</th>
+                  <td></td><td></td><td></td><td></td><td></td><td></td>
+                </tr>
+                <tr>
+                  <th>11:00 - 12:30</th>
+                  <td></td><td></td><td></td><td></td><td></td><td></td>
+                </tr>                
+                <tr>
+                  <th>12:30 - 14:00</th>
+                  <td></td><td></td><td></td><td></td><td></td><td></td>
+                </tr>     
+                <tr>
+                  <th>14:00 - 15:30</th>
+                  <td></td><td></td><td></td><td></td><td></td><td></td>
+                </tr> 
+                <tr>
+                  <th>15:30 - 17:00</th>
+                  <td></td><td></td><td></td><td></td><td></td><td></td>
+                </tr> 
+                <tr>
+                  <th>17:00 - 18:30</th>
+                  <td></td><td></td><td></td><td></td><td></td><td></td>
+                </tr> 
+                <tr>
+                  <th>18:30 - 20:00</th>
+                  <td></td><td></td><td></td><td></td><td></td><td></td>
+                </tr> 
+                <tr>
+                  <th>20:00 - 21:30</th>
+                  <td></td><td></td><td></td><td></td><td></td><td></td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
     </div>
   </section>
-
-
-<button data-bs-toggle="modal" data-bs-target="#parol_yangilash">sasa</button>
-
-<div class="modal" id="parol_yangilash" tabindex="-1">
-  <form action="#" method="post">
-    @csrf 
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Yangilash</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-          sasa
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Bekor qilish</button>
-          <button type="submit" class="btn btn-primary">Saqlash</button>
-        </div>
-      </div>
-    </div>
-  </form>
-</div>
-
 
 @endsection
